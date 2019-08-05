e2851e54bfb83634de8b5c3718b7cb97aeb1f6fb
Naveen Panwar <naveen.panwar27@gmail.com>
2019-08-05 06:37:47 +0530

    Created function to query the API with parameters

--- orders_wrapper.py
+++ orders_wrapper.py
@@ -2,6 +2,8 @@
 import urllib
 from urllib import request
 from decouple import config
+from datetime import datetime
+import time
 
 shop_address = config('SHOP_ADDRESS')
 access_token = config('ACCESS_TOKEN')
@@ -11,23 +13,69 @@ headers = {}
 headers["Content-Type"] = "application/graphql"
 headers["X-Shopify-Access-Token"] = access_token
 
-data = b"""
+query = """
 {
-    orders(first: 5) {
-      edges {
-        node {
-          id
-          email 
+    orders(first: 5 {query}) {
+        edges {
+            node {
+                id
+                processedAt
+                totalPriceSet {
+                    shopMoney {
+                        currencyCode
+                        amount
+                    }
+                }
+                name
+                displayFulfillmentStatus
+                customer {
+                    firstName
+                    lastName
+                }
+            }
         }
-      }
-      pageInfo {
-        hasNextPage
-      }
+        pageInfo {
+            hasNextPage
+        }
     }
 }
 """
 
-req = request.Request(api_url, data=data, headers=headers, method="POST")
+def Orders(min_processed_at=None, max_processed_at=None, fulfillment_status=None):
+    global query
+    data = qu = q = d = ""
+    if min_processed_at or max_processed_at or fulfillment_status:
+        q+= "query: \"{}\""
 
-response = urllib.request.urlopen(req)
-print(response.read().decode('utf-8'))
+    if min_processed_at:
+        d += "processed_at:>"+min_processed_at.strftime("%Y-%m-%dT%H-%M-%SZ")+" "
+
+    if max_processed_at:
+        d += "processed_at:<"+max_processed_at.strftime("%Y-%m-%dT%H-%M-%SZ")+" "
+
+    if fulfillment_status:
+        d += "fulfillment_status="+fulfillment_status
+
+    if d != "":
+        qu = q.format(d)
+
+    if qu != "":
+        qu = ", "+qu
+        data = query.replace("{query}",qu)
+    else:
+        data = query.replace("{query}","")
+
+    data = data.encode('utf-8')
+    req = request.Request(api_url, data=data, headers=headers, method="POST")
+
+    response = urllib.request.urlopen(req)
+    print(response.read().decode('utf-8'))
+
+now = datetime.now()
+time.sleep(2)
+now2 = datetime.now()
+Orders(min_processed_at=now,max_processed_at=now2,fulfillment_status="pending")
+Orders(min_processed_at=now)
+Orders(max_processed_at=now2)
+Orders(fulfillment_status="pending")
+Orders()
